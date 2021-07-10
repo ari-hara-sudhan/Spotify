f08f6279aef0c29ec28cd5a815d89417ae1618d9
=ari <=ahshari2003@gmail.com>
2021-07-10 12:06:57 +0530

    working on login

---
+++ src/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Login.css"
+function Login() {
+    return (
+        <div className="login">
+            <img src="https://wallpaperaccess.com/full/1373271.jpg"/>
+            <a>LOGIN WITH SPOTIFY</a>
+            
+        </div>
+    )
+}
+
+export default Login
